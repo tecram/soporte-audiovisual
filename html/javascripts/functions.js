c4a451b61b4c4831b1b1d33cace12d98bd9c5880
designfenix <info@design-fenix.com.ar>
2017-02-26 00:43:52 -0300

    - modificado el search - terminado el contacto - correccion de warnings - limpiado el JS

--- html/javascripts/functions.js
+++ html/javascripts/functions.js
@@ -2,20 +2,56 @@ var initPage = (function () {
 
 	'use strict';
 
-	var asdAfd;
+	var morphSearch = $('#morphsearch'),
+		input = $('input.morphsearch-input'),
+		ctrlClose = $('span.morphsearch-close'),
+		isOpen = false;
+
 	function asd() {
 		var asd = 1;
 	};
 
-	$(document).ready( function () {
-		asd();
-		$('.mobileToggle').on('click', function(e){ 
-			e.preventDefault();
-			$(this).toggleClass('active');
-			$('.main-nav').toggleClass('hidden-xs hidden-sm');
-		})
-	})
-	$(window).load(function(){
-		$('section.loader').fadeOut('fast');
-	})
+	function toggleSearch(evt){
+		if( evt.type.toLowerCase() === 'focus' && isOpen ) return false;
+		var offsets = morphsearch.getBoundingClientRect();
+		if( isOpen ) {
+			morphSearch.removeClass( 'open' );
+			if( input.value !== '' ) {
+				setTimeout(function() {
+					morphSearch.addClass( 'hideInput' );
+					setTimeout(function() {
+						morphSearch.removeClass( 'hideInput' );
+						input.value = '';
+					}, 300 );
+				}, 500);
+			}
+			
+			input.blur();
+		}
+		else {
+			morphSearch.addClass( 'open' );
+		}
+		isOpen = !isOpen;
+	};
+
+	$('.mobileToggle').on('click', function(e){ 
+		e.preventDefault();
+		$(this).toggleClass('active');
+		$('.main-nav').toggleClass('hidden-xs hidden-sm');
+	});
+
+	input.on( 'focus', toggleSearch );
+	ctrlClose.on( 'click', toggleSearch );
+	$(document).on( 'keydown', function( ev ) {
+		var keyCode = ev.keyCode || ev.which;
+		if( keyCode === 27 && isOpen ) {
+			toggleSearch(ev);
+		}
+	} );
+	
+});
+
+$(window).load( function() {
+	'use strict';
+	$('section.loader').delay(1000).fadeOut('fast');
 });
