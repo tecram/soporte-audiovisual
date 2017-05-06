18523a8ddfc9aa6501324a953e9c5a63df1fa873
designfenix <info@design-fenix.com.ar>
2017-05-06 00:17:39 -0300

    ultimos fixes

--- html/javascripts/functions.js
+++ html/javascripts/functions.js
@@ -1,69 +1,85 @@
+var SoporteAudio = {
+	wowInit: function() {
+		'use strict';
+		var wow = new WOW({
+			boxClass: 'wow',
+			animateClass: 'animated',
+			offset: 200,
+			mobile: true,
+			live: true
+		})
+		wow.init();
+	},
+	panelFiltrosInit: function(){
+		'use strict';
+		var panelContainer = $('.filter-container'),
+			panelContent = $('.filter-container .content'),
+			panelBtn = $('.show-filter'),
+			panelOverlay = $('.overlay-filter'),
+			flag = 1;
+		if(panelContainer.length) {
+			if(flag===1){
+				panelContent.nanoScroller();
+				flag = 2;
+			}
+			panelBtn.click( function (e) {
+				e.preventDefault();
+				panelContainer.toggleClass('active');
+				panelContent.toggleClass('active');
+				panelOverlay.toggleClass('active');
+			});
+			panelOverlay.click( function (e) {
+				e.preventDefault();
+				panelContainer.removeClass('active');
+				panelContent.removeClass('active');
+				panelOverlay.removeClass('active');
+			});
+		};
+	},
+	tabsDescription: function(element){
+		'use strict';
+		$('.nav-tabs a, .nav-tabs li, .tab-pane').removeClass('active');
+		var tabToShow = $(element).attr('href');
+		$(element).parent().addClass('active');
+		$(tabToShow).addClass('active');
+	},
+	getFiltersParams: function () {
+		'use strict';
+		var filters = [],
+			id;
+
+		$.each($('.filter-container input:checked'), function () {
+			id = $(this).attr('id');
+			filters.push(id);
+		});
+		SoporteAudio.loading('show');
+		$.ajax({
+			method: "POST",
+			url: path + "/getProducts.php",
+			data: { products: filters.join() }
+		})
+		.done(function( response ) {
+		    $('.main-content').html(response);
+		    SoporteAudio.loading('hide');
+		    $('.overlay-filter').trigger('click');
+	  	});
+	},
+	loading: function(status){
+		'use strict';
+		var $loadContent = $('section.loader');
+		if(status==='show'){
+			$loadContent.fadeIn('fast');
+		}else if(status==='hide'){
+			$loadContent.fadeOut('fast');
+		}else{
+			return('Loading Error: pasar parametro "show/hide"');
+		}
+	}
+};
+
 var initPage = (function () {
 
 	'use strict';
-
-	var SoporteAudio = {
-		wowInit: function() {
-			var wow = new WOW({
-				boxClass: 'wow',
-				animateClass: 'animated',
-				offset: 200,
-				mobile: true,
-				live: true
-			})
-			wow.init();
-		},
-		panelFiltrosInit: function(){
-			var panelContainer = $('.filter-container'),
-				panelContent = $('.filter-container .content'),
-				panelBtn = $('.show-filter'),
-				panelOverlay = $('.overlay-filter'),
-				flag = 1;
-			if(panelContainer.length) {
-				if(flag===1){
-					panelContent.nanoScroller();
-					flag = 2;
-				}
-				panelBtn.click( function (e) {
-					e.preventDefault();
-					panelContainer.toggleClass('active');
-					panelContent.toggleClass('active');
-					panelOverlay.toggleClass('active');
-				});
-				panelOverlay.click( function (e) {
-					e.preventDefault();
-					panelContainer.removeClass('active');
-					panelContent.removeClass('active');
-					panelOverlay.removeClass('active');
-				});
-			};
-		},
-		tabsDescription: function(element){
-			$('.nav-tabs a, .nav-tabs li, .tab-pane').removeClass('active');
-			var tabToShow = $(element).attr('href');
-			$(element).parent().addClass('active');
-			$(tabToShow).addClass('active');
-		},
-		getFiltersParams: function () {
-			var filters = [],
-				id;
-
-			$.each($('.filter-container input:checked'), function () {
-				id = $(this).attr('id');
-				filters.push(id);
-			});
-			
-			$.ajax({
-  				method: "POST",
-				url: path + "/getProducts.php",
-				data: { products: filters.join() }
-	        })
-			.done(function( response ) {
-			    $('.main-content').html(response);
-			    $('.overlay-filter').trigger('click');
-		  	});
-		}
-	}
 	
 	SoporteAudio.panelFiltrosInit();
 
@@ -96,5 +112,5 @@ var initPage = (function () {
 
 $(window).load( function() {
 	'use strict';
-	$('section.loader').delay(1000).fadeOut('fast');
+	SoporteAudio.loading('hide');
 });
